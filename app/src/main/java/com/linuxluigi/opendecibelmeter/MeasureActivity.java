b329da10756e11a19971f47d12dea24c39be06f5
Steffen Exler <steffen.exler@gmail.com>
2018-06-22 14:40:56 +0200

    update measure ui

--- app/src/main/java/com/linuxluigi/opendecibelmeter/MeasureActivity.java
+++ app/src/main/java/com/linuxluigi/opendecibelmeter/MeasureActivity.java
@@ -24,15 +24,6 @@ public class MeasureActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -53,28 +44,6 @@ public class MeasureActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.measure, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -86,11 +55,13 @@ public class MeasureActivity extends AppCompatActivity
             Intent i = new Intent(getApplicationContext(), LoginActivity.class);
             startActivity(i);
         } else if (id == R.id.nav_gallery) {
-            // go to login activity
+            // go to profile activity
             Intent i = new Intent(getApplicationContext(), ProfileActivity.class);
             startActivity(i);
         } else if (id == R.id.nav_slideshow) {
-
+            // go to log activity
+            Intent i = new Intent(getApplicationContext(), ViewLogActivity.class);
+            startActivity(i);
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
