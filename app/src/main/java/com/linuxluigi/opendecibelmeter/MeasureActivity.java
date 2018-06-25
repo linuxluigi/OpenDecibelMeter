0b327f2ae75f7069a61a65098f6390d211164bbc
Steffen Exler <steffen.exler@gmail.com>
2018-06-25 12:21:58 +0200

    add get location

--- app/src/main/java/com/linuxluigi/opendecibelmeter/MeasureActivity.java
+++ app/src/main/java/com/linuxluigi/opendecibelmeter/MeasureActivity.java
@@ -1,8 +1,12 @@
 package com.linuxluigi.opendecibelmeter;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -11,11 +15,17 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MeasureActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    // location
+    EditText latituteEditText;
+    EditText longitudeEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +42,13 @@ public class MeasureActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // set ui Elements to private vars
+        latituteEditText = findViewById(R.id.positionLatitude);
+        longitudeEditText = findViewById(R.id.positionLongitude);
+
+        // set action listener
+        setButtons();
     }
 
     @Override
@@ -73,5 +90,55 @@ public class MeasureActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void setButtons() {
+        // get current Position Button
+        Button positionBtn = findViewById(R.id.positionBtn);
+        positionBtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, getResources().getString(R.string.update_location), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                LocationManager locationManager = (LocationManager)
+                        getSystemService(Context.LOCATION_SERVICE);
+
+                LocationListener locationListener = new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+
+                    }
+
+                    @Override
+                    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String s) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String s) {
+
+                    }
+                };
+
+                assert locationManager != null;
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+
+
+                Location location = locationManager.getLastKnownLocation((LocationManager.GPS_PROVIDER));
+                double latitude = location.getLatitude();
+                double logitute = location.getLongitude();
+
+                latituteEditText.setText(String.valueOf(latitude));
+                longitudeEditText.setText(String.valueOf(logitute));
+            }
+        });
     }
 }
